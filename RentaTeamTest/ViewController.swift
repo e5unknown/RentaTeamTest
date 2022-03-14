6435b04babda6cf685d158a126099e7cec4d6bb7
e5unknown <e5unknown@ya.ru>
2022-03-14 22:25:47 +0300

    Initial Commit

---
+++ RentaTeamTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RentaTeamTest
+//
+//  Created by Azat Battalov on 14.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
